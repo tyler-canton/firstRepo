aa1c80aaab58db29cefd6d3d344eb1472e1a7074
tylor-canton <tylor.canton@conduent.com>
2021-02-11 23:37:29 -0500

    Fundamental training

--- src/app.js
+++ src/app.js
@@ -1,4 +1,65 @@
 import 'bootstrap'
 import './sass/main.scss'
-let name  = 'Tyler Canton';
-console.log(name);
+
+// VARIABLE: Var Let Const
+// COMMENTS
+// DECLARATIONS
+// SCOPE
+// HOISTING
+// GLOBAL SCOPE 
+
+ let fname = 'Yunis';
+ let anoterVariable = 'fname';
+// LEXICAL OR OBJECT SCOPE
+let obj = {
+    // local scope
+    fname:'Ty'
+};
+
+let dataRelations=[{
+	"userId": 1,
+	"id": 1,
+	"title": "delectus aut autem",
+	"completed": false
+},{
+	"userId": 1,
+	"id": 1,
+	"title": "delectus aut autem",
+	"completed": false
+},{
+	"userId": 1,
+	"id": 1,
+	"title": "delectus aut autem",
+	"completed": false
+},{
+	"userId": 1,
+	"id": 1,
+	"title": "delectus aut autem",
+	"completed": false
+},{
+	"userId": 1,
+	"id": 1,
+	"title": "delectus aut autem",
+	"completed": false
+}];
+
+person = obj;
+// Undefined
+console.log(person[anoterVariable]);
+console.log(person.fname);
+console.log(obj.fname);
+
+
+// This is an comment
+
+/**
+ *  @obj 
+ *  @property firstname: this param with a datatype of string
+ *  @TODO : I need to do addt'l work here
+ */
+
+
+
+
+let name  = 'Ty Canton';
+
