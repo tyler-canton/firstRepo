40da8b33bb20f404d3783631ba043a976cb910f0
Christina Slocumb <chrislo.cs27@gmail.com>
2021-02-15 14:44:51 -0500

    JS Dice Game

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
-import 'bootstrap'
-import './sass/main.scss'
+roll = () => {
+    return Math.ceil(Math.random() * 6)
+}
+game = () => {
+    player1dice = roll();
+    player2dice = roll();
 
-//Global Variables//
+    console.log('Player 1 rolls a ', player1dice);
+    console.log('Player 2 rolls a ', player2dice);
 
-let name  = 'The Joker';
-
-// This is an object
-let obj = {
-    firstName: 'Harley Quinn'
-};
-
-let arr = ['Tylahn','Chris','Quinn'];
+    if(player1dice > player2dice) {
+        return'player 1 wins!!!';
+    }
+  else if (player1dice < player2dice) {
+      return 'player 2 wins!!!';
+  }
+  else {
+      return 'tie, you both win'
+  }
+} 
+play = () => {
+    result = game();
+    while(confirm(`${result} Play Again?`)) {
+        result = game();
+    }
+}
