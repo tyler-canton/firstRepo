f765abbedce511be4f9abe95e7f03307c4ddf050
tylor-canton <tylor.canton@conduent.com>
2021-02-15 16:27:57 -0500

    Debugging and make functions global because webpack makes the app private

--- src/app.js
+++ src/app.js
@@ -1,26 +1,36 @@
-roll = () => {
-    return Math.ceil(Math.random() * 6)
-}
-game = () => {
-    player1dice = roll();
-    player2dice = roll();
+import 'bootstrap'
+import './sass/main.scss'
 
-    console.log('Player 1 rolls a ', player1dice);
-    console.log('Player 2 rolls a ', player2dice);
+// These ES^ arrow functions are not declared
+// Webpack Dev will wrapper your app to make it private
+// Use 'window' to make them global 
+// You need to declare your variables
 
-    if(player1dice > player2dice) {
-        return'player 1 wins!!!';
-    }
-  else if (player1dice < player2dice) {
-      return 'player 2 wins!!!';
-  }
-  else {
-      return 'tie, you both win'
-  }
-} 
-play = () => {
-    result = game();
-    while(confirm(`${result} Play Again?`)) {
-        result = game();
-    }
+
+window.roll = () => {
+	return Math.ceil(Math.random() * 6)
 }
+window.game = () => {
+	let player1dice = roll();
+	let player2dice = roll();
+
+	console.log('Player 1 rolls a ', player1dice);
+	console.log('Player 2 rolls a ', player2dice);
+
+	if (player1dice > player2dice) {
+		return 'player 1 wins!!!';
+	}
+	else if (player1dice < player2dice) {
+		return 'player 2 wins!!!';
+	}
+	else {
+		return 'tie, you both win'
+	}
+}
+window.play = () => {
+	let result = game();
+	while (confirm(`${result} Play Again?`)) {
+		result = game();
+	}
+}
+window.namer = 'tyler';
