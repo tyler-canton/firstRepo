e88b613292ee516fcfacf8261658c343b4da8587
tylor-canton <tylor.canton@conduent.com>
2021-02-17 22:43:34 -0500

    new commmit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'bootstrap'
 import './sass/main.scss'
+import { moduleObj } from './module';
 
 // VARIABLE: Var Let Const
 // COMMENTS
@@ -8,35 +9,35 @@ import './sass/main.scss'
 // HOISTING
 // GLOBAL SCOPE 
 
- let fname = 'Yunis';
- let anoterVariable = 'fname';
+let fname = 'Yunis';
+let anoterVariable = 'fname';
 // LEXICAL OR OBJECT SCOPE
 let obj = {
-    // local scope
-    fname:'Ty'
+	// local scope
+	fname: 'Ty'
 };
 
-let dataRelations=[{
+let dataRelations = [{
 	"userId": 1,
 	"id": 1,
 	"title": "delectus aut autem",
 	"completed": false
-},{
+}, {
 	"userId": 1,
 	"id": 1,
 	"title": "delectus aut autem",
 	"completed": false
-},{
+}, {
 	"userId": 1,
 	"id": 1,
 	"title": "delectus aut autem",
 	"completed": false
-},{
+}, {
 	"userId": 1,
 	"id": 1,
 	"title": "delectus aut autem",
 	"completed": false
-},{
+}, {
 	"userId": 1,
 	"id": 1,
 	"title": "delectus aut autem",
@@ -46,7 +47,7 @@ let dataRelations=[{
 let person = obj;
 // Undefined
 console.log(person[anoterVariable]);
-console.log(person.fname);
+console.log(moduleObj);
 
 
 
@@ -61,26 +62,26 @@ console.log(person.fname);
 
 
 
-let name  = 'Ty Canton';
+let name = 'Ty Canton';
 
 /**
  * // Global module
 var myModule = (function ( jQ, _ ) {
-  
-    function privateMethod1(){
-        jQ(".container").html("test");
-    }
 
-    function privateMethod2(){
-      console.log( _.min([10, 5, 100, 2, 1000]) );
-    }
-    
-    return{
-        publicMethod: function(){
-            privateMethod1();                
-        }            
-    };
-   
+	function privateMethod1(){
+		jQ(".container").html("test");
+	}
+
+	function privateMethod2(){
+	  console.log( _.min([10, 5, 100, 2, 1000]) );
+	}
+
+	return{
+		publicMethod: function(){
+			privateMethod1();
+		}
+	};
+
 // Pull in jQuery and Underscore
 }( jQuery, _ ));
 
